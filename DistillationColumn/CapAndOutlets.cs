b1a98f7688336751f51a3b76a644e2fca7b76f6f
supriyachaugule1 <supriya.chaugule@cctech.co.in>
2023-01-17 19:24:27 +0530

    Support brackets of rectangular platform

--- DistillationColumn/CapAndOutlets.cs
+++ DistillationColumn/CapAndOutlets.cs
@@ -8,6 +8,7 @@ using Tekla.Structures.Model;
 using TSM = Tekla.Structures.Model;
 using T3D = Tekla.Structures.Geometry3d;
 using Tekla.Structures.Geometry3d;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace DistillationColumn
 {
@@ -18,12 +19,16 @@ namespace DistillationColumn
 
         double elevation=68000;
         List<List<double>> _platformList;
+
+
         public CapAndOutlets(Globals global, TeklaModelling tModel)
         {
             _global = global;
             _tModel = tModel;
             _platformList = new List<List<double>>();
+
             createCapAndOutlets();
+            capBrackets();
         }
         public void createCapAndOutlets()
         {
@@ -39,6 +44,7 @@ namespace DistillationColumn
             TSM.ContourPoint capTop = _tModel.ShiftVertically(point1, radius);
             TSM.ContourPoint capBottom = _tModel.ShiftVertically(point1, -radius);
             _global.Position.Plane = TSM.Position.PlaneEnum.MIDDLE;
+            _global.Position.Depth = TSM.Position.DepthEnum.MIDDLE;
             string profile = "CAP" + diameter;
             Beam cap = _tModel.CreateBeam(capTop, capBottom, profile, "IS2062", "8", _global.Position, "cap");
 
@@ -66,7 +72,10 @@ namespace DistillationColumn
 
                 }
             }
+            
         }
+
+       
 
         public void Cuts(T3D.Point p1,T3D.Point p2,double radius)
         {
@@ -77,5 +86,60 @@ namespace DistillationColumn
 
             }
         }
+
+        public void capBrackets()
+        {
+            double radius = _tModel.GetRadiusAtElevation(elevation, _global.StackSegList, true);
+            TSM.ContourPoint origin = new TSM.ContourPoint(_global.Origin, null);
+            TSM.ContourPoint point1 = _tModel.ShiftVertically(origin, elevation);
+            TSM.ContourPoint capTop = _tModel.ShiftVertically(point1, radius);
+            TSM.ContourPoint point5 = _tModel.ShiftHorizontallyRad(capTop, 800, 1, 0 * (Math.PI / 180));
+            for (int i = 1; i <= 4; i++)
+            {
+                if ((i * 90) <= 360)
+                {
+                    double ang = i * (90 * (Math.PI / 180));
+                    if (ang >= 90 * (Math.PI / 180))
+                    {
+                        _global.Position.Rotation = TSM.Position.RotationEnum.TOP;
+                    }
+                    if (ang >= 180 * (Math.PI / 180))
+                    {
+                        _global.Position.Rotation = TSM.Position.RotationEnum.BACK;
+                    }
+                    if (ang >= 270 * (Math.PI / 180))
+                    {
+                        _global.Position.Rotation = TSM.Position.RotationEnum.BELOW;
+                    }
+                    if (ang >= 360 * (Math.PI / 180))
+                    {
+                        _global.Position.Rotation = TSM.Position.RotationEnum.FRONT;
+                    }
+                    TSM.ContourPoint capBracketBottom = _tModel.ShiftAlongCircumferenceRad(point5, ang, 1);
+                    TSM.ContourPoint capBracketTop = _tModel.ShiftVertically(capBracketBottom, 600);
+                    _tModel.CreateBeam(new T3D.Point(capBracketBottom.X, capBracketBottom.Y, capBracketBottom.Z - 500), capBracketTop, "ISMC100", "IS2062", "2", _global.Position, "");
+
+
+                }
+            }
+
+            TSM.ContourPoint point6 = _tModel.ShiftHorizontallyRad(capTop, 1200, 1, 45 * (Math.PI / 180));
+            for (int i = 0; i < 4; i++)
+            {
+                if ((i * 90) <= 360)
+                {
+                    double ang = i * (-90 * (Math.PI / 180));
+                    _global.Position.Rotation = TSM.Position.RotationEnum.FRONT;
+                    TSM.ContourPoint capPlatformBracketBottom = _tModel.ShiftAlongCircumferenceRad(point6, ang, 1);
+                    TSM.ContourPoint capPlatformBracketTop = RectangularPlatform._platformPointList[i];
+                    _tModel.CreateBeam(new T3D.Point(capPlatformBracketBottom.X, capPlatformBracketBottom.Y, capPlatformBracketBottom.Z - 1000), capPlatformBracketTop, "ISMC100", "IS2062", "2", _global.Position, "");
+
+
+                }
+            }
+
+        }
+
+
     }
 }
--- DistillationColumn/RectangularPlatform.cs
+++ DistillationColumn/RectangularPlatform.cs
@@ -9,9 +9,13 @@ using T3D = Tekla.Structures.Geometry3d;
 using Tekla.Structures.Model;
 using Newtonsoft.Json.Linq;
 using System.Runtime.CompilerServices;
-using Render;
+//using Render;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 using System.IO.Ports;
+using Tekla.Structures.Geometry3d;
+using System.Collections;
+using Tekla.Structures.Model.Operations;
+
 namespace DistillationColumn
 {
     internal class RectangularPlatform
@@ -26,7 +30,11 @@ namespace DistillationColumn
         string profile1;
         List<List<double>> _platformList;
         List<double> _distanceLengthList;
-        List<TSM.ContourPoint> _platformPointList;
+        static public List<TSM.ContourPoint> _platformPointList;
+        List<Point> Intersect;
+        ArrayList myList = new ArrayList();
+        List<Part> _platformBracketParts;
+        List<Point> my = new List<Point>();
         public RectangularPlatform(Globals global, TeklaModelling tModel)
         {
             _global = global;
@@ -34,12 +42,17 @@ namespace DistillationColumn
             _platformList = new List<List<double>>();
             _distanceLengthList = new List<double>();
             _platformPointList = new List<TSM.ContourPoint>();
+            Intersect = new List<Point>();
+            _platformBracketParts = new List<Part>();
+
 
             SetPlatformData();
-            createPlatform();
-            CreateHandRailParallelToXAxis();
-            CreateHandRailParallelToYAxis();
+            //createPlatform();
+            //CreateHandRailParallelToXAxis();
+            //CreateHandRailParallelToYAxis();
             CreateSupportPlate();
+            createSupportBrackets();
+            createSupportBrackets1();
         }
 
         public void SetPlatformData()
@@ -51,7 +64,7 @@ namespace DistillationColumn
                 height = (float)platform["height"];
                 width = (float)platform["width"];
                 plateWidth = (float)platform["plateWidth"];
-                _platformList.Add(new List<double> { elevation,  height , width,plateWidth });
+                _platformList.Add(new List<double> { elevation, height, width, plateWidth });
             }
 
         }
@@ -67,8 +80,8 @@ namespace DistillationColumn
 
                 for (int i = 0; i <= number; i++)
                 {
-                    if ((i + 1)* plateWidth <= width)
-                    { 
+                    if ((i + 1) * plateWidth <= width)
+                    {
                         profile1 = "PL" + plateWidth + "*20";
                     }
                     else
@@ -80,6 +93,7 @@ namespace DistillationColumn
                     T3D.Point start = new T3D.Point(point2.X - i * plateWidth, point2.Y, point2.Z);
                     T3D.Point end = new T3D.Point(point2.X - i * plateWidth, point2.Y + height / 2, point2.Z);
                     T3D.Point end2 = new T3D.Point(point2.X - i * plateWidth, -(point2.Y + height / 2), point2.Z);
+                    _global.Position.Depth = TSM.Position.DepthEnum.FRONT;
                     _global.Position.Plane = TSM.Position.PlaneEnum.LEFT;
 
                     Beam beamRight = _tModel.CreateBeam(start, end, profile1, "IS2062", "5", _global.Position, "");
@@ -89,7 +103,7 @@ namespace DistillationColumn
                     _global.platformParts.Add(beamLeft);
 
 
-                    
+
                 }
             }
         }
@@ -108,7 +122,7 @@ namespace DistillationColumn
                 if (j == 0)
                 {
                     sPoint = new ContourPoint(new T3D.Point((origin.X + (width / 2)), origin.Y, origin.Z), null);
-                    sPoint = new ContourPoint(new T3D.Point((sPoint.X - 250 - (_distanceLengthList[0] / 2)-50), sPoint.Y, sPoint.Z), null);
+                    sPoint = new ContourPoint(new T3D.Point((sPoint.X - 250 - (_distanceLengthList[0] / 2) - 50), sPoint.Y, sPoint.Z), null);
                     ePoint = new ContourPoint(new T3D.Point(sPoint.X, sPoint.Y + (height / 2), sPoint.Z), null);
                 }
 
@@ -116,7 +130,7 @@ namespace DistillationColumn
                 {
 
                     sPoint = new ContourPoint(new T3D.Point((origin.X - (width / 2)), origin.Y, origin.Z), null);
-                    sPoint = new ContourPoint(new T3D.Point((sPoint.X + 250 + (_distanceLengthList[0] / 2)+50), sPoint.Y, sPoint.Z), null);
+                    sPoint = new ContourPoint(new T3D.Point((sPoint.X + 250 + (_distanceLengthList[0] / 2) + 50), sPoint.Y, sPoint.Z), null);
                     ePoint = new ContourPoint(new T3D.Point(sPoint.X, sPoint.Y - (height / 2), sPoint.Z), null);
 
 
@@ -189,7 +203,7 @@ namespace DistillationColumn
                     }
 
 
-                   
+
 
                     CPart.SetInputPositions(sPoint, ePoint);
 
@@ -369,7 +383,7 @@ namespace DistillationColumn
         public void CreateSupportPlate()
         {
             TSM.ContourPoint origin = _tModel.ShiftVertically(_global.Origin, elevation);
-            TSM.ContourPoint point1 = new ContourPoint(new T3D.Point((origin.X + (width / 2)), origin.Y, origin.Z - 7.7), null);
+            TSM.ContourPoint point1 = new ContourPoint(new T3D.Point((origin.X + (width / 2)), origin.Y, origin.Z), null);
             point1.Y += (height / 2);
             _platformPointList.Add(point1);
             TSM.ContourPoint point2 = new ContourPoint(new T3D.Point(point1.X, (point1.Y - height), point1.Z), null);
@@ -391,12 +405,12 @@ namespace DistillationColumn
                 {
                     _platformPointList[i].Y += 25;
                     _platformPointList[i + 1].Y -= 25;
-                    
+
 
                     _tModel.CreateBeam(_platformPointList[i], _platformPointList[i + 1], _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam" + (i + 1));
                     _platformPointList[i].Y -= 25;
                     _platformPointList[i + 1].Y += 25;
-                   
+
                 }
                 else if (i == 1)
                 {
@@ -409,7 +423,7 @@ namespace DistillationColumn
                 else if (i == 3)
                 {
                     _platformPointList[i].X -= 25;
-                    _platformPointList[0].X += 50;
+                    _platformPointList[0].X += 25;
                     _tModel.CreateBeam(_platformPointList[i], _platformPointList[0], _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam" + (i + 1));
                     _platformPointList[i].X += 25;
                     _platformPointList[0].X -= 25;
@@ -427,14 +441,163 @@ namespace DistillationColumn
             }
         }
 
-       
+
+
+        public void createSupportBrackets()
+        {
+            _global.ProfileStr = "ISMC100";
+            _global.Position.Plane = Position.PlaneEnum.MIDDLE;
+            _global.Position.Depth = Position.DepthEnum.BEHIND;
+            _global.Position.Rotation = Position.RotationEnum.TOP;
+            _global.ClassStr = "2";
+
+            TSM.ContourPoint origin = _tModel.ShiftVertically(_global.Origin, elevation);
+            TSM.ContourPoint point1 = _tModel.ShiftHorizontallyRad(origin, width / 2, 1);
+            TSM.ContourPoint point2 = new ContourPoint(new T3D.Point(point1.X - 200, point1.Y + height / 2, point1.Z), null);
+            TSM.ContourPoint point3 = new ContourPoint(new T3D.Point(point2.X, (point2.Y - height), point2.Z), null);
+            Beam p1 = _tModel.CreateBeam(point2, point3, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+            //Intersection(p1);
+            _platformBracketParts.Add(p1);
+
+
+            TSM.ContourPoint point21 = new ContourPoint(new T3D.Point(point1.X - width + 200, point1.Y + height / 2, point1.Z), null);
+            TSM.ContourPoint point31 = new ContourPoint(new T3D.Point(point21.X, (point21.Y - height), point21.Z), null);
+            Beam p2 = _tModel.CreateBeam(point21, point31, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+            _platformBracketParts.Add(p2);
+
+            double no = (width - 400) / 1000;
+            var x = (no - Math.Truncate(no)) * 1000;
+            if (x > 200)
+            {
+                double no1 = Math.Truncate(no);
+
+                for (int i = 1; i <= no1; i++)
+                {
+
+                    TSM.ContourPoint point4 = new ContourPoint(new T3D.Point(point2.X - i * 1000, point2.Y, point2.Z), null);
+                    TSM.ContourPoint point5 = new ContourPoint(new T3D.Point(point3.X - i * 1000, (point3.Y), point3.Z), null);
+                    Beam p3 = _tModel.CreateBeam(point4, point5, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                    _platformBracketParts.Add(p3);
+                }
+            }
+            else
+            {
+                double no1 = Math.Truncate(no);
+
+                for (int i = 1; i <= no1 - 1; i++)
+                {
+
+                    TSM.ContourPoint point4 = new ContourPoint(new T3D.Point(point2.X - i * 1000, point2.Y, point2.Z), null);
+                    TSM.ContourPoint point5 = new ContourPoint(new T3D.Point(point3.X - i * 1000, (point3.Y), point3.Z), null);
+                    Beam p4 = _tModel.CreateBeam(point4, point5, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                    _platformBracketParts.Add(p4);
+                }
+                double dist = x + 1000;
+                dist = dist / 2;
+
+                TSM.ContourPoint point41 = new ContourPoint(new T3D.Point(point2.X - (no1 - 1) * 1000 - dist, point2.Y, point2.Z), null);
+                TSM.ContourPoint point51 = new ContourPoint(new T3D.Point(point3.X - (no1 - 1) * 1000 - dist, (point3.Y), point3.Z), null);
+                Beam p5 = _tModel.CreateBeam(point41, point51, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                _platformBracketParts.Add(p5);
+            }
+        }
+
+        public void createSupportBrackets1()
+        {
+            TSM.ContourPoint origin = _tModel.ShiftVertically(_global.Origin, elevation);
+            TSM.ContourPoint point1 = _tModel.ShiftHorizontallyRad(origin, width / 2, 1);
+            TSM.ContourPoint point2 = new ContourPoint(new T3D.Point(point1.X, (point1.Y - height / 2) + 200, point1.Z), null);
+            TSM.ContourPoint point3 = new ContourPoint(new T3D.Point(point2.X - width, (point2.Y), point2.Z), null);
+            Beam p1 = _tModel.CreateBeam(point2, point3, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+            _platformBracketParts.Add(p1);
+            Intersection(point2, point3);
+
+            TSM.ContourPoint point21 = new ContourPoint(new T3D.Point(point1.X, (point1.Y + height / 2) - 200, point1.Z), null);
+            TSM.ContourPoint point31 = new ContourPoint(new T3D.Point(point21.X - width, (point21.Y), point21.Z), null);
+            Beam p2 = _tModel.CreateBeam(point21, point31, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+            _platformBracketParts.Add(p2);
+            Intersection(point21, point31);
+
+            double no = (height - 400) / 1000;
+            var x = (no - Math.Truncate(no)) * 1000;
+            if (x > 200)
+            {
+                double no1 = Math.Truncate(no);
+
+                for (int i = 1; i <= no1; i++)
+                {
+
+                    TSM.ContourPoint point4 = new ContourPoint(new T3D.Point(point2.X, point2.Y + i * 1000, point2.Z), null);
+                    TSM.ContourPoint point5 = new ContourPoint(new T3D.Point(point3.X, point3.Y + i * 1000, point3.Z), null);
+                    Beam p3 = _tModel.CreateBeam(point4, point5, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                    _platformBracketParts.Add(p3);
+                    Intersection(point4, point5);
+                }
+            }
+            else
+            {
+                double no1 = Math.Truncate(no);
+
+                for (int i = 1; i <= no1 - 1; i++)
+                {
+                    TSM.ContourPoint point4 = new ContourPoint(new T3D.Point(point2.X, point2.Y + i * 1000, point2.Z), null);
+                    TSM.ContourPoint point5 = new ContourPoint(new T3D.Point(point3.X, point3.Y + i * 1000, point3.Z), null);
+                    Beam p4 = _tModel.CreateBeam(point4, point5, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                    _platformBracketParts.Add(p4);
+                    Intersection(point4, point5);
+
+                }
+                double dist = x + 1000;
+                dist = dist / 2;
+
+                TSM.ContourPoint point41 = new ContourPoint(new T3D.Point(point2.X, point2.Y + (no1 - 1) * 1000 + dist, point2.Z), null);
+                TSM.ContourPoint point51 = new ContourPoint(new T3D.Point(point3.X, point3.Y + (no1 - 1) * 1000 + dist, point3.Z), null);
+                Beam p5 = _tModel.CreateBeam(point41, point51, _global.ProfileStr, "ASTMA106", _global.ClassStr, _global.Position, "beam");
+                _platformBracketParts.Add(p5);
+
+                Intersection(point41, point51);
+            }
+
+
+
+        }
+
+        public void Intersection(Point p1, Point p2)
+        {
+            foreach (Beam part in _platformBracketParts)
+            {
+                Solid Solid = part.GetSolid();
+                myList = Solid.Intersect(p1, p2);
+                if (myList.Count == 0)
+                    break;
+
+                for (int i = 0; i < myList.Count; i++)
+                {
+                    Point intersect = (Point)myList[i];
+                    Intersect.Add(intersect);
+                }
+                foreach (var p in Intersect)
+                {
+
+                    //Beam b1;
+                    Operation.Split(part, p);
+                    _tModel.Model.CommitChanges();
+                    //_platformBracketParts.Add(b1)
+                }
+                //Operation.Split(part,intersect);
+                //_tModel.Model.CommitChanges();
+
+            }
+
+
+        }
 
     }
 
 
-        
+
 
 
 }
-       
+
 
