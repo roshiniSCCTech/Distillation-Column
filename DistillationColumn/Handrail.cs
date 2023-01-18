944be1e792638a350dc167e3a8ff90695dc28158
abhijitpatil181 <abhijit.patil@cctech.co.in>
2023-01-18 18:00:52 +0530

    added function to read json data for handrail

--- DistillationColumn/Handrail.cs
+++ DistillationColumn/Handrail.cs
@@ -10,6 +10,7 @@ using Tekla.Structures.Model;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 using Tekla.Structures.Geometry3d;
 using Tekla.Structures;
+using Render;
 
 namespace DistillationColumn
 {
@@ -19,104 +20,127 @@ namespace DistillationColumn
     TeklaModelling _tModel;
 
 
-    double ladderOrientation = 0;
-    double platFormStartAngle = 59;
-    double platFormEndAngle = 300;
-    double elevation = 26000;
-    double platformLength = 1000;
-    double distanceFromStack = 200;
+    double ladderOrientation ;
+    double platFormStartAngle;
+    double platFormEndAngle ;
+    double elevation ;
+    double platformLength ;
+    double distanceFromStack;
     double gratingOuterRadius;
+    double gratingThickness;
     List<double> arcLengthList = new List<double>();
+    List<List<double>> handRailData;
 
     public Handrail(Globals global, TeklaModelling tModel)
     {
       _global = global;
       _tModel = tModel;
+      handRailData= new List<List<double>>();
+      SetHandrailrData();
 
-      createHandrail();
+        foreach (List<double> handrail in handRailData)
+        {
+            platFormStartAngle = handrail[0];
+            platFormEndAngle = handrail[1];
+            elevation = handrail[2];
+            //  width = (float)handrail["Platform_Width"];
+            platformLength = handrail[3];
+            distanceFromStack = handrail[4];
+            ladderOrientation = handrail[5];
+            gratingThickness= handrail[6];
+             createHandrail();
+
+        }
     }
 
-    //public void SetHandrailrData()
-    //{
-    //  List<JToken> HandrailList = _global.jData["Handrail"].ToList();
-    //  foreach (JToken handrail in HandrailList)
-    //  {
-    //    orientationAngle = (float)ladder["Orientation_Angle"];
-    //    elevation = (float)ladder["Elevation"];
-    //    width = (float)ladder["Width"];
-    //    height = (float)ladder["Height"];
-    //    rungSpacing = (float)ladder["Rungs_spacing"];
-    //    _ladderList.Add(new List<double> { orientationAngle, elevation, width, height, rungSpacing });
-    //  }
+    public void SetHandrailrData()
+    {           
+           
+        List<JToken> _handrailList = _global.JData["Ladder"].ToList();
+        foreach (JToken handrail in _handrailList)
+        {
+            platFormStartAngle = (float)handrail["Platform_Start_Angle"];
+            platFormEndAngle = (float)handrail["Platfrom_End_Angle"];
+            elevation = (float)handrail["Elevation"];
+            //  width = (float)handrail["Platform_Width"];
+            platformLength = (float)handrail["Platform_Length"];
+            distanceFromStack = (float)handrail["Distance_From_Stack"];
+            ladderOrientation = (float)handrail["Orientation_Angle"];
+            gratingThickness = (float)handrail["Grating_Thickness"];
+            handRailData.Add(new List<double>()
+            { platFormStartAngle, platFormEndAngle, elevation,  platformLength, distanceFromStack, ladderOrientation,gratingThickness });
+        }
+            
+    }
 
     void createHandrail1()
     {
-      CustomPart handrail = new CustomPart();
-      handrail.Name = "Final_HandRail";
-      handrail.Number = BaseComponent.CUSTOM_OBJECT_NUMBER;
-      //origin for handrail
-      TSM.ContourPoint point1 = _tModel.ShiftVertically(_global.Origin, elevation);
+        CustomPart handrail = new CustomPart();
+        handrail.Name = "Final_HandRail";
+        handrail.Number = BaseComponent.CUSTOM_OBJECT_NUMBER;
+        //origin for handrail
+        TSM.ContourPoint point1 = _tModel.ShiftVertically(_global.Origin, elevation);
 
 
-      TSM.ContourPoint point2 = _tModel.ShiftHorizontallyRad(point1, gratingOuterRadius, 1, platFormStartAngle * (Math.PI / 180));
+        TSM.ContourPoint point2 = _tModel.ShiftHorizontallyRad(point1, gratingOuterRadius, 1, platFormStartAngle * (Math.PI / 180));
 
-      //second point for handrail
-      point2 = _tModel.ShiftAlongCircumferenceRad(point2, 275, 2);
+        //second point for handrail
+        point2 = _tModel.ShiftAlongCircumferenceRad(point2, 275, 2);
 
 
 
-      for (int i = 0; i < arcLengthList.Count; i++)
-      {
+        for (int i = 0; i < arcLengthList.Count; i++)
+        {
         //if only one distance available
         if (arcLengthList.Count == 1)
         {
-          if (arcLengthList[i] > 1100 && arcLengthList[i] <= 2600)
-          {
+            if (arcLengthList[i] > 1100 && arcLengthList[i] <= 2600)
+            {
             arcLengthList.Add(arcLengthList[i] - 600);
             arcLengthList.RemoveAt(i);
-          }
+            }
 
-          if (arcLengthList[i] > 2500)
-          {
+            if (arcLengthList[i] > 2500)
+            {
             arcLengthList.Add((arcLengthList[i] - 1100) / 2);
             arcLengthList.Add((arcLengthList[i] - 1100) / 2);
             arcLengthList.RemoveAt(i + 1);
-          }
+            }
 
-          //To create only single handrail minimum 1100 distance is required 
-          if (arcLengthList[i] < 1100)
-          {
+            //To create only single handrail minimum 1100 distance is required 
+            if (arcLengthList[i] < 1100)
+            {
             break;
-          }
+            }
         }
 
         //check the distance of last handrail and according to that either modify second last or keep as it is
         if (i + 1 == arcLengthList.Count - 1)
         {
-          if (arcLengthList[i + 1] >= 1200 && arcLengthList[i + 1] <= 2600)
-          {
+            if (arcLengthList[i + 1] >= 1200 && arcLengthList[i + 1] <= 2600)
+            {
             arcLengthList.Add(arcLengthList[i + 1] - 600);
             arcLengthList.RemoveAt(i + 1);
-          }
+            }
 
 
-          if (arcLengthList[i + 1] > 2500)
-          {
+            if (arcLengthList[i + 1] > 2500)
+            {
             double sum = (arcLengthList[i + 1] + arcLengthList[i]) / 2;
             arcLengthList.RemoveAt(i + 1);
             arcLengthList.Add(sum);
             arcLengthList.Add(sum);
 
-          }
+            }
 
-          if (arcLengthList[i + 1] > 600 && arcLengthList[i + 1] < 1200)
-          {
+            if (arcLengthList[i + 1] > 600 && arcLengthList[i + 1] < 1200)
+            {
             double sum = (arcLengthList[i + 1] + arcLengthList[i]) / 2;
             arcLengthList.RemoveAt(i);
             arcLengthList.RemoveAt(i);
             arcLengthList.Add(sum);
             arcLengthList.Add(sum - 600);
-          }
+            }
 
         }
 
@@ -125,7 +149,7 @@ namespace DistillationColumn
 
         if (i > 0)
         {
-          point2 = _tModel.ShiftAlongCircumferenceRad(point2, arcLengthList[i - 1] + 600, 2);
+            point2 = _tModel.ShiftAlongCircumferenceRad(point2, arcLengthList[i - 1] + 600, 2);
         }
 
 
@@ -144,8 +168,8 @@ namespace DistillationColumn
 
 
 
-      }
-      _tModel.Model.CommitChanges();
+        }
+        _tModel.Model.CommitChanges();
     }
 
     void createHandrail()
@@ -154,7 +178,7 @@ namespace DistillationColumn
       double orientationAngle = 0 * Math.PI / 180;
       double radius = _tModel.GetRadiusAtElevation(elevation, _global.StackSegList);
       gratingOuterRadius = radius + distanceFromStack + platformLength;
-      createGrating();
+      //createGrating();
       double totalArcLength;
       double totalAngle = platFormEndAngle - platFormStartAngle;
       totalArcLength = Math.Abs(2 * Math.PI * gratingOuterRadius * (totalAngle / 360));
@@ -182,30 +206,6 @@ namespace DistillationColumn
       }
 
     }
-    void createGrating()
-    {
-      List<TSM.ContourPoint> _pointsList = new List<ContourPoint>();
-
-      double midAngle = ((platFormEndAngle + platFormStartAngle) / 2);
-
-      TSM.ContourPoint origin = new TSM.ContourPoint(_global.Origin, null);
-      TSM.ContourPoint point1 = _tModel.ShiftVertically(origin, elevation);
-      TSM.ContourPoint startPoint = _tModel.ShiftHorizontallyRad(point1, gratingOuterRadius, 1, platFormStartAngle * Math.PI / 180);
-      TSM.ContourPoint midPoint = _tModel.ShiftHorizontallyRad(point1, gratingOuterRadius, 1, (midAngle * Math.PI / 180));
-      midPoint = new ContourPoint(midPoint, new TSM.Chamfer(0, 0, TSM.Chamfer.ChamferTypeEnum.CHAMFER_ARC_POINT));
-      TSM.ContourPoint endPoint = _tModel.ShiftHorizontallyRad(point1, gratingOuterRadius, 1, (platFormEndAngle * Math.PI / 180));
-
-
-      //_global.ProfileStr = "PL" + "1200" + "*25";
-      //_global.ClassStr = "10";
-      _global.Position.Plane = TSM.Position.PlaneEnum.LEFT;
-      _global.Position.Rotation = TSM.Position.RotationEnum.FRONT;
-      _global.Position.Depth = TSM.Position.DepthEnum.BEHIND;
-      _pointsList.Add(startPoint);
-      _pointsList.Add(midPoint);
-      _pointsList.Add(endPoint);
-      _tModel.CreatePolyBeam(_pointsList, "PL1200*25", "IS2062", "5", _global.Position, "");
-
-    }
+   
   }
 }
