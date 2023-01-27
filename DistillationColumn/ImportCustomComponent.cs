45c3c5b6fb221ccc3378625d7c857c5385990a93
abhijitpatil181 <abhijit.patil@cctech.co.in>
2023-01-27 12:29:15 +0530

    created branch

---
+++ DistillationColumn/ImportCustomComponent.cs
@@ -0,0 +1,34 @@
+﻿using HelperLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+using Tekla.Structures.Catalogs;
+
+namespace DistillationColumn
+{
+    internal class ImportCustomComponent
+    {
+        Globals _global;
+        TeklaModelling _tModel;
+        public ImportCustomComponent(Globals global, TeklaModelling tModel)
+        {
+            _global = global;
+            _tModel = tModel;
+            ImportComponent();
+
+        }
+
+        void ImportComponent()
+        {
+            CatalogHandler c=new CatalogHandler();
+            c.ImportCustomComponentItems(@"D:\\custom_components\\");
+           // if (new CatalogHandler().ImportCustomComponentItems("D:\\custom_components"))
+                //Console.WriteLine("Custom components imported successfully to catalog.");
+        }
+    }
+
+   
+}
