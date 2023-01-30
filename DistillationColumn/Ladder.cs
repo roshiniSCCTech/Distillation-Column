f97d8a9997f948428a5d3ab234e2584653cda602
PatilBhavita <bhavita.patil@cctech.co.in>
2023-01-30 13:13:41 +0530

    ladderHoop profile issue resolved

--- DistillationColumn/Ladder.cs
+++ DistillationColumn/Ladder.cs
@@ -196,7 +196,7 @@ namespace DistillationColumn
                 if (Height > 3000)
                 {
                     Detail D = new Detail();
-                    D.Name = "ladderHoop1";
+                    D.Name = "LadderHoop";
                     D.Number = BaseComponent.CUSTOM_OBJECT_NUMBER;
                     D.LoadAttributesFromFile("standard");
                     D.UpVector = new Vector(0, 0, 0);
